70ad971e0966d53fa7c8928ac88e546f167fa0ac
CarlosGeorge <carlosgeorge248@gmail.com>
2021-10-01 10:08:51 -0300

    Feature: Health Check

--- src/test/java/br/com/til/tasks/functional/tests/TasksTest.java
+++ src/test/java/br/com/til/tasks/functional/tests/TasksTest.java
@@ -31,7 +31,8 @@ public class TasksTest {
 		 
 		 
 		
-		webDriver.navigate().to("http:/192.168.0.8:8001/tasks");
+//		webDriver.navigate().to("http:/192.168.0.8:8001/tasks");
+		webDriver.navigate().to("http:/192.168.15.3.:8003/tasks");
 		
 		webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
